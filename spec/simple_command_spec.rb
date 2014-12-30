29b28b0aa7dfd1bb90cfc02581a92568ddb04ae5
Andrea Pavoni <andrea.pavoni@gmail.com>
2014-12-30 15:00:04 +0100

    refactors specs

--- spec/simple_command_spec.rb
+++ spec/simple_command_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe SimpleCommand do
   let(:command) { SuccessCommand.new(2) }
   let(:fail_command) { FailCommand.new(2) }
-  let(:missed_perform_command) { MissedPerformCommand.new(2) }
 
   describe '#perform' do
+    let(:missed_perform_command) { MissedPerformCommand.new(2) }
+
     it 'returns the command object' do
       expect(command.perform).to be_a(SuccessCommand)
     end
@@ -16,14 +17,12 @@ describe SimpleCommand do
   end
 
   describe '#success?' do
-    context 'when #perform was already called' do
-      it 'is true by default' do
-        expect(command.perform.success?).to be_truthy
-      end
+    it 'is true by default' do
+      expect(command.perform.success?).to be_truthy
+    end
 
-      it 'is false if something went wrong' do
-        expect(fail_command.perform.success?).to be_falsy
-      end
+    it 'is false if something went wrong' do
+      expect(fail_command.perform.success?).to be_falsy
     end
 
     context 'when perform is not called yet' do
@@ -33,37 +32,25 @@ describe SimpleCommand do
     end
   end
 
-  describe '#errors' do
-    context 'with no errors' do
-      it 'returns an empty Hash' do
-        errors = command.perform.errors
-
-        expect(errors).to be_empty
-      end
+  describe '#result' do
+    it 'returns the result of command execution' do
+      expect(command.perform.result).to eq(4)
     end
 
-    context 'with errors' do
-      let(:errors) { fail_command.perform.errors }
-
-      it 'returns an Hash' do
-        expect(errors).to_not be_empty
-      end
-
-      it 'has a key with error message' do
-        expect(errors[:wrong_math]).to_not be_nil
+    context 'when perform is not called yet' do
+      it 'returns nil' do
+        expect(command.result).to be_nil
       end
     end
   end
 
   describe '#failure?' do
-    context 'when #perform was already called' do
-      it 'is false by default' do
-        expect(command.perform.failure?).to be_falsy
-      end
+    it 'is false by default' do
+      expect(command.perform.failure?).to be_falsy
+    end
 
-      it 'is true if something went wrong' do
-        expect(fail_command.perform.failure?).to be_truthy
-      end
+    it 'is true if something went wrong' do
+      expect(fail_command.perform.failure?).to be_truthy
     end
 
     context 'when perform is not called yet' do
@@ -73,16 +60,24 @@ describe SimpleCommand do
     end
   end
 
-  describe '#result' do
-    context 'when #perform was already called' do
-      it 'returns the result of command execution' do
-        expect(command.perform.result).to eq(4)
+  describe '#errors' do
+    let(:errors) { command.perform.errors }
+
+    it 'returns an Hash' do
+      expect(errors).to be_a(Hash)
+    end
+
+    context 'with no errors' do
+      it 'returns an empty Hash' do
+        expect(errors).to be_empty
       end
     end
 
-    context 'when perform is not called yet' do
-      it 'returns nil' do
-        expect(command.result).to be_nil
+    context 'with errors' do
+      let(:errors) { fail_command.perform.errors }
+
+      it 'has a key with error message' do
+        expect(errors[:wrong_math]).to_not be_nil
       end
     end
   end
