9575fa5a737ee5cf4bf01b3e461a087502d78750
Vanboom <dvboom@hotmail.com>
2018-02-22 13:55:18 -0600

    implemented full_messages similar to ActiveRecord Errors

--- lib/simple_command/errors.rb
+++ lib/simple_command/errors.rb
@@ -19,5 +19,17 @@ module SimpleCommand
         self[field].each { |message| yield field, message }
       end
     end
+
+    def full_messages
+      map { |attribute, message| full_message(attribute, message) }
+    end
+
+    private
+    def full_message(attribute, message)
+      return message if attribute == :base
+      attr_name = attribute.to_s.tr('.', '_').capitalize
+      "%s %s" % [attr_name, message]
+    end
+
   end
 end
